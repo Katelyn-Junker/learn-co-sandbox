e6f0c7d74abfc0652355cf8613000273ec7102f3
Katelyn Junker <kjunker@villanova.edu>
2018-11-07 00:58:46 +0000

    automatically backed up by learn

--- week-8/index.js
+++ week-8/index.js
@@ -10,7 +10,7 @@ submitButton.addEventListener('click', (event) =>{
       const year = document.querySelector('#year')
       
       title.innerText = movieJson['Title']
-      title.innerText = movieJson['Title']
-      
+      rated.innerText = movieJson['Rated']
+      year.innerText = movieJson['Year']
     })
 })
