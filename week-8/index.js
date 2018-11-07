03019217304d56dfd255745fc585a5da72bdb093
Katelyn Junker <kjunker@villanova.edu>
2018-11-07 00:56:32 +0000

    automatically backed up by learn

---
+++ week-8/index.js
@@ -0,0 +1,10 @@
+const submitButton= document.querySelector('submit')
+submitButton.addEventListener('click', (event) =>{
+  const input = document.querySelector('input')
+  const movieName = input.value
+  fetch('http://www.omdbapi.com/?apikey=c87ebb66&t=' + movieName)
+    .then((response) =>response.json)
+    .then((movieJson) =>{
+      const title = document.querySelector('#title')
+    })
+})
